4fb27777da63c41cc412592f4f6c219bba3079f2
mehdighemati <mehdi.ghemati@gmail.com>
2020-02-01 09:23:16 -0800

    optimiser

--- main.py
+++ main.py
@@ -14,13 +14,43 @@ import torch.nn.functional as F
 
 class Net(nn.Module):
     def __init__(self):
-        super(nn.Module,self).__init__()
+        super().__init__()
         self.fc1 = nn.Linear(28*28, 64)
         self.fc2 = nn.Linear(64, 64)
         self.fc3 = nn.Linear(64, 64)
         self.fc4 = nn.Linear(64, 10)
 
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return F.log_softmax(x, dim=1)
+
 
 net = Net()
 
 print(net)
+
+X = torch.rand((28,28))
+X = X.view(-1,28*28)
+output= net(X)
+print(output)
+
+import torch.optim as optim
+
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+EPOCHS = 3
+
+for epoch in range(EPOCHS):
+    for data in trainset:
+        # data is a batch of featuresets and labels
+        X, y = data
+        net.zero_grad()
+        output = net(X.view(-1,28*28))
+        loss = F.nll_loss(output, y)
+        loss.backward()
+        optimizer.step()
+    print(loss)
