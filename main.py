9215e092ab8da88c2d87a205c368d10fab2b5cc7
mehdighemati <mehdi.ghemati@gmail.com>
2020-02-01 06:18:55 -0800

    gitignore fixes

---
+++ main.py
@@ -0,0 +1,13 @@
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+from torchvision import transforms, datasets
+
+train = datasets.MNIST("", train=True, download=True, transform = transforms.Compose([transforms.ToTensor()]))
+test = datasets.MNIST("", train=False, download=True, transform = transforms.Compose([transforms.ToTensor()]))
+
+trainset = torch.utils.data.DataLoader(train, batch_size=10, shuffle=True)
+testset = torch.utils.data.DataLoader(test, batch_size=10, shuffle=True)
+
+import torch.nn as nn
+import torch.nn.functional as F
